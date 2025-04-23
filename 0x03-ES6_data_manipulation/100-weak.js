c7fed23bd008c1a6e26656273129511c70d203a0
Elfenom <elphenom10@gmail.com>
2025-04-23 23:06:01 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,13 @@
-export default function updateUniqueItems(items) {
-  if (!(items instanceof Map)) {
-    throw new Error('Cannot process');
+export const weakMap = new WeakMap();
+
+const MAX_ENDPOINT_CALLS = 5;
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
-  for (const [key, value] of items) {
-    if (value === 1) {
-      items.set(key, 100);
-    }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
   }
-  return items;
 }
